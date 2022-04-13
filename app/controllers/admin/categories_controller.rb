58c0d1681db6be2a1a138542c8ef2a1d1b4471be
Marcos Spomberg <marcosspomberg@gmail.com>
2022-04-13 21:10:59 +0000

    Create admin/categories controller methods

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,24 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_USER'], 
+                               password: ENV['HTTP_BASIC_PASSWORD']
+
+  def index
+    @category = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(:name)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: "Category created!"
+    else
+      render :new
+    end
+    
+  end
+
+end
