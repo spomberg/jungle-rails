2768af83a6a53d5d7290d0377bdb3e2176e9a98a
Marcos Spomberg <marcosspomberg@gmail.com>
2022-04-13 21:23:40 +0000

    Fix admin/category routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, only: [:index, :show, :create]
+    resources :categories, only: [:index, :show, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
