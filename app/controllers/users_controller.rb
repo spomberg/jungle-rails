269ea023a6379e19fa9c19f3d8a720fdcd908403
Marcos Spomberg <marcosspomberg@gmail.com>
2022-04-14 21:12:10 +0000

    Edit users controller to keep users from signing up with a repeated email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,12 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
 
-    if user.save
+    unless User.find_by(email: user.email)
+      user.save
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to 'signup'
+      redirect_to '/signup', notice: "Error: couldn't complete signup process, please try again"
     end
   end
 
