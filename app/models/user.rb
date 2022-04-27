4f6901bcc6ac359f78ae02a67a52235553dea6bc
Marcos Spomberg <marcosspomberg@gmail.com>
2022-04-27 21:34:13 +0000

    Add password length validation to user model and password length test

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
   validates :password_confirmation, presence: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
     context "given that all fields are entered correctly" do
       it "saves successfully" do
-        user = User.new(name: "abc", email: "abc@gmail.com", password: "abc", password_confirmation: "abc").save
+        user = User.new(name: "abc", email: "abc@gmail.com", password: "12345678", password_confirmation: "12345678").save
         expect(user).to eq(true)
       end
     end
 
     context "given that the password and password confirmation don't match" do
       it "doesn't save successfully" do
-        user = User.new(name: "abc", email: "abc@gmail.com", password: "abc", password_confirmation: "abc1")
+        user = User.new(name: "abc", email: "abc@gmail.com", password: "12345678", password_confirmation: "12345677")
         validation = user.save
         expect(validation).to eq(false)
         expect(user.errors[:password_confirmation]).to eq(["doesn't match Password"])
@@ -23,13 +23,13 @@ RSpec.describe User, type: :model do
         user = User.new(name: "abc", email: "abc@gmail.com", password: nil, password_confirmation: nil)
         validation = user.save
         expect(validation).to eq(false)
-        expect(user.errors[:password]).to eq(["can't be blank", "can't be blank"])
+        expect(user.errors[:password]).to eq(["can't be blank", "can't be blank", "is too short (minimum is 8 characters)"])
       end
     end
 
     context "given that the password confirmation field is blank" do
       it "doesn't save successfully" do
-        user = User.new(name: "abc", email: "abc@gmail.com", password: "abc", password_confirmation: nil)
+        user = User.new(name: "abc", email: "abc@gmail.com", password: "12345678", password_confirmation: nil)
         validation = user.save
         expect(validation).to eq(false)
         expect(user.errors[:password_confirmation]).to eq(["can't be blank"])
@@ -38,7 +38,7 @@ RSpec.describe User, type: :model do
 
     context "given that name field is blank" do
       it "doesn't save successfully" do
-        user = User.new(name: nil, email: "abc@gmail.com", password: "abc", password_confirmation: "abc")
+        user = User.new(name: nil, email: "abc@gmail.com", password: "12345678", password_confirmation: "12345678")
         validation = user.save
         expect(validation).to eq(false)
         expect(user.errors[:name]).to eq(["can't be blank"])
@@ -47,7 +47,7 @@ RSpec.describe User, type: :model do
 
     context "given that email field is blank" do
       it "doesn't save successfully" do
-        user = User.new(name: "abc", email: nil, password: "abc", password_confirmation: "abc")
+        user = User.new(name: "abc", email: nil, password: "12345678", password_confirmation: "12345678")
         validation = user.save
         expect(validation).to eq(false)
         expect(user.errors[:email]).to eq(["can't be blank"])
@@ -56,8 +56,8 @@ RSpec.describe User, type: :model do
 
     context "given that the email entered is not unique" do
       it "doesn't save successfully" do
-        user1 = User.new(name: "abc", email: "abc@gmail.com", password: "abc", password_confirmation: "abc").save
-        user2 = User.new(name: "abc", email: "abc@gmail.com", password: "abc", password_confirmation: "abc")
+        user1 = User.new(name: "abc", email: "abc@gmail.com", password: "12345678", password_confirmation: "12345678").save
+        user2 = User.new(name: "abc", email: "abc@gmail.com", password: "12345678", password_confirmation: "12345678")
         validation = user2.save
         expect(validation).to eq(false)
         expect(user2.errors[:email]).to eq(["has already been taken"])
@@ -66,11 +66,20 @@ RSpec.describe User, type: :model do
 
     context "given that the email entered has been used but is in upper case" do
       it "doesn't save successfully" do
-        user1 = User.new(name: "abc", email: "abc@gmail.com", password: "abc", password_confirmation: "abc").save
-        user2 = User.new(name: "abc", email: "ABC@GMAIL.COM", password: "abc", password_confirmation: "abc")
+        user1 = User.new(name: "abc", email: "abc@gmail.com", password: "12345678", password_confirmation: "12345678").save
+        user2 = User.new(name: "abc", email: "ABC@GMAIL.COM", password: "12345678", password_confirmation: "12345678")
         validation = user2.save
         expect(validation).to eq(false)
         expect(user2.errors[:email]).to eq(["has already been taken"])
+      end
+    end
+
+    context "password has less than 8 characters" do
+      it "doesn't save successfully" do
+        user = User.new(name: "abc", email: "abc@gmail.com", password: "1234567", password_confirmation: "1234567")
+        validation = user.save
+        expect(validation).to eq(false)
+        expect(user.errors[:password]).to eq(["is too short (minimum is 8 characters)"])
       end
     end
   end
