afdc41a1fd38032d1e05326c2ec27e4584c3ec14
Marcos Spomberg <marcosspomberg@gmail.com>
2022-04-27 20:56:02 +0000

    Add tests for user models happy path and password confirmation not matching

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "given that all fields are entered correctly" do
+    it "saves successfully" do
+      user = User.new(name: "abc", email: "abc@gmail.com", password: "abc", password_confirmation: "abc").save
+      expect(user).to eq(true)
+    end
+  end
+
+  context "given that the password and password confirmation don't match" do
+    it "not save successfully" do
+      user = User.new(name: "abc", email: "abc@gmail.com", password: "abc", password_confirmation: "abc1")
+      validation = user.save
+      expect(validation).to eq(false)
+      expect(user.errors[:password_confirmation]).to eq(["doesn't match Password"])
+    end
+  end
 end
