caed0a59810e3af73df92c48a749dc73799d86ee
Marcos Spomberg <marcosspomberg@gmail.com>
2022-04-27 20:32:50 +0000

    Add name and price fields tests for the Product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,5 +10,27 @@ RSpec.describe Product, type: :model do
         expect(product).to eq(true)
       end
     end
+
+    context "given that the name field is missing" do
+      it "should not save" do
+        cat1 = Category.new
+        cat1.save
+        product = Product.new(name: nil, price_cents: 12, quantity: 2, category: cat1)
+        validation = product.validate
+        expect(validation).to eq(false) 
+        expect(product.errors[:name]).to eq(["can't be blank"]) 
+      end
+    end
+
+    context "given that the price field is missing" do
+      it "should not save" do
+        cat1 = Category.new
+        cat1.save
+        product = Product.new(name: "abc", price_cents: nil, quantity: 2, category: cat1)
+        validation = product.validate
+        expect(validation).to eq(false) 
+        expect(product.errors[:price]).to eq(["is not a number", "can't be blank"]) 
+      end
+    end
   end
 end
