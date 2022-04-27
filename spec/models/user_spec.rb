0773d62145b0fc9755e0acf9937658076681c3aa
Marcos Spomberg <marcosspomberg@gmail.com>
2022-04-27 21:26:45 +0000

    Add case sensitive setting to email uniqueness validation and test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe User, type: :model do
       end
     end
 
-    context "given that the password confirmation field is blank " do
+    context "given that the password confirmation field is blank" do
       it "doesn't save successfully" do
         user = User.new(name: "abc", email: "abc@gmail.com", password: "abc", password_confirmation: nil)
         validation = user.save
@@ -36,7 +36,7 @@ RSpec.describe User, type: :model do
       end
     end
 
-    context "given that name field is blank " do
+    context "given that name field is blank" do
       it "doesn't save successfully" do
         user = User.new(name: nil, email: "abc@gmail.com", password: "abc", password_confirmation: "abc")
         validation = user.save
@@ -45,7 +45,7 @@ RSpec.describe User, type: :model do
       end
     end
 
-    context "given that email field is blank " do
+    context "given that email field is blank" do
       it "doesn't save successfully" do
         user = User.new(name: "abc", email: nil, password: "abc", password_confirmation: "abc")
         validation = user.save
@@ -54,10 +54,20 @@ RSpec.describe User, type: :model do
       end
     end
 
-    context "given that the email entered is not unique " do
+    context "given that the email entered is not unique" do
       it "doesn't save successfully" do
         user1 = User.new(name: "abc", email: "abc@gmail.com", password: "abc", password_confirmation: "abc").save
         user2 = User.new(name: "abc", email: "abc@gmail.com", password: "abc", password_confirmation: "abc")
+        validation = user2.save
+        expect(validation).to eq(false)
+        expect(user2.errors[:email]).to eq(["has already been taken"])
+      end
+    end
+
+    context "given that the email entered has been used but is in upper case" do
+      it "doesn't save successfully" do
+        user1 = User.new(name: "abc", email: "abc@gmail.com", password: "abc", password_confirmation: "abc").save
+        user2 = User.new(name: "abc", email: "ABC@GMAIL.COM", password: "abc", password_confirmation: "abc")
         validation = user2.save
         expect(validation).to eq(false)
         expect(user2.errors[:email]).to eq(["has already been taken"])
